98330123681165c6dde9b8be52b812b88e4b6671
sakshijagtap1226 <sakshijagtap1226@gmail.com>
2026-06-02 13:07:01 +0530

    hii

---
+++ internship-day-7/script.js
@@ -0,0 +1,10 @@
+let num1=30
+let num2=20
+let sum=num1+num2
+console.log("The sum of "+num1+" and "+num2+" is: "+sum)        
+let difference=num1-num2
+console.log("The difference of "+num1+" and "+num2+" is: "+difference)        
+let product=num1*num2       
+console.log("The product of "+num1+" and "+num2+" is: "+product)
+let quotient=num1/num2
+console.log("The quotient of "+num1+" and "+num2+" is: "+quotient)
